df2193358ebf178beb949d6503e6bf5afe454a5e
geopreece <77272498+geopreece@users.noreply.github.com>
2021-01-11 15:00:41 +0000

    Add files via upload

--- Game/test.py
+++ Game/test.py
@@ -2,79 +2,94 @@ from time import sleep
 import sys
 
 # function which asks a question
-def question(question, correct_answer, wrong_answer, expected_ans):
+def question(question, correct_answer, wrong_answer, expected_answer):
 
-# for loop prints question on the screen
     for i in question:
 
         sys.stdout.write(i)
         sys.stdout.flush()
         sleep(0.03)
 
-    ans = input('\n\nEnter your answer here :') # get answer from contestant
+    ans = input('\n\nEnter your answer here : ') # get answer from contestant
     ans = ans.lower()
-    if ans == expected_ans:          # NOTE this line matches the correct answer in the question
-        for i in correct_answer: # run if answer is correct
+    if ans == expected_answer:          # NOTE this line matches the correct answer in the question
+        for i in correct_answer: # NOTE: Run if answer is correct
             sys.stdout.write(i)
             sys.stdout.flush()
             sleep(0.03)
     else:
-        for i in wrong_answer:  # run if answer is wrong
+        for i in wrong_answer:  # NOTE: Run if answer is wrong
             sys.stdout.write(i)
             sys.stdout.flush()
             sleep(0.03)
 
 question('''
-What body parts do northern leopard frogs use to help swallow their prey?
+Who is England's all time top goal scorer?
 
-A)Feet                         B)Eyes
+A)  Bobby CHarlton      B)  Gary Lineker
 
-C)Ears                         D)Nostrils
+C)  Wayne Rooney        D)  Alan Shearer
+
 ''','''
-Correct! Northern leopard frogs use their ears to help swallow their prey.
+Correct! Wayne Rooney overtook Bobby Charlton's recored of 49 international goals on Tuesday 8th September 2015.
 ''','''
-Wrong!  Northern leopard frogs use their ears to help swallow their prey.
-''','''b''')
+Wrong! Wayne Rooney overtook Bobby Charlton's recored of 49 international goals on Tuesday 8th September 2015.
+''',
+'c')
 
-# question('''What colour is the solid form of oxygen?
-# A)Red                          B)Green
+question('''
+Who holds the record for most appearances in the Premier League?
 
-# C)Yellow                       D)Blue
-# ''',''' 
-# Correct! The solid colour for of oxygen is infact blue.
-#  ''','''
-# Wrong! The solid colour for of oxygen is infact blue''','''d''')
+A)  Ryan Giggs          B)  Gareth Barry
 
-# question('''
-# What colour is the sunset on Mars?
-# A)Red                          B)Green
+C)  James Milner        D)  David James
 
-# C)Blue                         D)Orange
-# ''','''
-# Correct! The sunset on mars is blue (And really cool!).
-# ''','''
-# Wrong! The sunset on mars is blue (And really cool!).
-# ''','''c''')
+''','''
+Correct! Gareth Barry holds the record after 653 career Premier League appearances for Aston Villa, Manchester City, Everton & West Brom.
+''','''
+Wrong! Gareth Barry holds the record after 653 career Premier League appearances for Aston Villa, Manchester City, Everton & West Brom.
+''',
+'b')
 
-# question('''
-# Which part of the human body does not have a blood supply?
-# A) Kneecap                    B)Earlobe
+question('''
+Who was the first person in space?
 
-# C)Fingernails                 D)Cornea
-# ''','''
-# Correct! The cornea have no blood supply!.
-# ''','''
-# Wrong! The cornea have no blood supply!.
-# ''','''d''')
+A)  Yuri gagarin        B) Buzz Aldrin
 
-# question('''
-# In 2015, Merlin the rescue cat set a new world record for what?
-# A)Longest Fur                 B)Loudest Purr
+C)  Neil Armstrong      D) Alan Shepherd
 
-# C)Best Mouser                 D)Heaviest Cat
-# ''','''
-# Correct! The most adorable world record ever for the loudest purr.
-# ''','''
-# Wrong! The most adorable world record ever for the loudest purr.
-# ''','''b''')
+''','''
+Correct! Although Neil Armstrong was the first man on the moon and Alan Shepherd was the first American in space, Yuri Gagarin was the first person to go into space.
+''','''
+Wrong! Although Neil Armstrong was the first man on the moon and Alan Shepherd was the first American in space, Yuri Gagarin was the first person to go into space.
+''',
+'a')
+
+question('''
+Which nation has won the most World Cups?
+
+A)  Italy               B) France
+
+C)  Germany             D) Brazil
+
+''','''
+Correct! Brazil have won the most World Cups with 5, despite not winning any of the last 4 tournaments.
+''','''
+Wrong! Brazil have won the most World Cups with 5, despite not winning any of the last 4 tournaments.
+''',
+'d')
+
+question('''
+Which of these is not a metal?
+
+A)  Magnesium           B) Lithium
+
+C)  Uranium             D) Nitrogen
+
+''','''
+Correct! Nitrogen is the only element that is not a metal.
+''','''
+Wrong! Nitrogen is the only element that is not a metal.
+''',
+'d')
 
