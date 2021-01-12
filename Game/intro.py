51ce2995013678477b9e890522918b09aedc8c8f
geopreece <77272498+geopreece@users.noreply.github.com>
2021-01-12 10:09:51 +0000

    Add files via upload

---
+++ Game/intro.py
@@ -0,0 +1,8 @@
+print("Welcome to the Haunted Mansion.")
+name = str(input("To pass into the building the spirits demand a name. Reveal your identity."))
+play = str(input.lower(f."So... {name}... \n Are you brave enough to enter the Haunted mansion? Once you go in there is no turning back."))
+
+if play in ["yes", "y"]:
+    # Hundreds of lines of glorious code.
+else:
+    print("Get a grip you scared little biiiitch.")
