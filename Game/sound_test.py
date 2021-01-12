5d5a1b1c81fc155b9bfbc82d2f9712800a4322af
kelseysheridan <kelsi0@live.com>
2021-01-12 14:28:45 +0000

    Sound!

---
+++ Game/sound_test.py
@@ -0,0 +1,11 @@
+from playsound import playsound
+
+playsound('ding.wav')
+
+# import os 
+# duration = 1
+# frequence = 440
+# os.system("play -nq -t alsa synth {} sine {}".format(duration,frequence))
+
+
+# print('\007')
